c25212b70e274b8f02984eedd5079d4a3ef8730d
Hanno Schlichting <hanno@hannosch.eu>
2007-06-06 08:44:05 +0000

    Let me run the tests in my buildout...

--- src/five/localsitemanager/tests.py
+++ src/five/localsitemanager/tests.py
@@ -3,7 +3,9 @@ from Testing.ZopeTestCase import ZopeDocFileSuite
 from Testing.ZopeTestCase import FunctionalDocFileSuite
 
 def test_suite():
-    if __name__ not in ('five.localsitemanager', '__main__'):
+    if __name__ not in ('five.localsitemanager',
+                        'five.localsitemanager.tests',
+                        '__main__'):
         # a safety net for when five.localsitemanager is manged into sys.path
         # of a zope2 product
         return unittest.TestSuite()

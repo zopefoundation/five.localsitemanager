55448af414268b42dc7989dbeb5c971a1a08fccc
Jens W. Klein <jk@kleinundpartner.at>
2019-08-13 14:56:55 +0200

    check if aq_parent isnt enough here (None check)

--- src/five/localsitemanager/utils.py
+++ src/five/localsitemanager/utils.py
@@ -1,8 +1,9 @@
-from zope.location.interfaces import IRoot
-
-from Acquisition import aq_parent, aq_inner
+# -*- coding: utf-8 -*-
+from Acquisition import aq_inner
+from Acquisition import aq_parent
 
 _marker = object()
+
 
 def get_parent(obj, default=_marker):
     """Returns the container the object was traversed via.
@@ -11,9 +12,6 @@ def get_parent(obj, default=_marker):
     Raises TypeError if the object doesn't have enough context to get the
     parent.
     """
-    if IRoot.providedBy(obj):
-        return None
-
     parent = aq_parent(aq_inner(obj))
     if parent is not None:
         return parent
